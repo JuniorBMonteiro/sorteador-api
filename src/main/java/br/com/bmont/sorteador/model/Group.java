9263dff1a3c907ee86f1a8410753e093c88c401f
JuniorBMonteiro <juniorbmonteiro@hotmail.com>
2022-03-11 20:45:07 -0300

    Modelagem das entidades

---
+++ src/main/java/br/com/bmont/sorteador/model/Group.java
@@ -0,0 +1,25 @@
+package br.com.bmont.sorteador.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Group {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @OneToMany
+    private List<Participant> participants;
+}
---
+++ src/main/java/br/com/bmont/sorteador/model/Participant.java
@@ -0,0 +1,24 @@
+package br.com.bmont.sorteador.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Group group;
+}
